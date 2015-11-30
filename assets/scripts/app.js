1e781a88cd4ea0fe65e652661cd0a4f8ba1a6d56
Christopher Mosure <cj@webmachine.io>
2015-11-30 11:29:53 -0500

    React elements as blog articles and cleaner implementation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,45 +1,50 @@
-// tutorial1-raw.js
-var CommentBox = React.createClass({
+var data = [
+  {id: 1, title: "Article Title Number 1", text: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the sublime of her own road, the Line Lane. Pitiful a rhetoric question ran over her cheek."},
+  {id: 2, title: "Article Title Number 2", text: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the sublime of her own road, the Line Lane. Pitiful a rhetoric question ran over her cheek."},
+  {id: 3, title: "Article Title Number 3", text: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the sublime of her own road, the Line Lane. Pitiful a rhetoric question ran over her cheek."},
+  {id: 4, title: "Article Title Number 4", text: "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the sublime of her own road, the Line Lane. Pitiful a rhetoric question ran over her cheek."}
+];
+
+var Articles = React.createClass({
   render: function() {
     return (
-      <div className="comment-box">
-        <h1>Comments</h1>
-        <CommentList />
-        <CommentForm />
-      </div>
-    );
-  }
-});
-// tutorial2.js
-var CommentList = React.createClass({
-  render: function() {
-    return (
-      <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      <div className="article-box">
+        <h2 className="page-title">Articles</h2>
+        <ArticleList data={this.props.data} />
       </div>
     );
   }
 });
 
-var CommentForm = React.createClass({
+var ArticleList = React.createClass({
   render: function() {
+    var articleNodes = this.props.data.map(function(article) {
+      return (
+        <Article title={article.title} key={article.id}>
+          {article.text}
+        </Article>
+      );
+    });
     return (
-      <div className="comment-form">
-        Hello, world! I am a CommentForm.
+      <div className="article-list">
+        {articleNodes}
       </div>
     );
   }
 });
 
-var Comment = React.createClass({
+var Article = React.createClass({
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return { __html: rawMarkup };
+  },
   render: function() {
     return (
-      <div className="comment">
-        <h2 className="commentAuthor">
-          {this.props.author}
-        </h2>
-        {this.props.children}
+      <div className="article">
+        <h3 className="article-title">
+          {this.props.title}
+        </h3>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
@@ -47,6 +52,6 @@ var Comment = React.createClass({
 
 
 ReactDOM.render(
-  React.createElement(CommentBox, null),
+  <Articles data={data} />,
   document.getElementById('content')
 );
